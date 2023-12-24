537dea7f256f7d48c5aaf6614162456c2def62a4
Dmitriy B. <dbkkll@yandex.ru>
2023-12-24 17:49:37 +0500

    Improve error handling & Cleanup

--- src/main.rs
+++ src/main.rs
@@ -24,63 +24,75 @@ fn main() {
 }
 
 fn run() -> Result<()> {
-    let matches = app::matches();
-
     let shmarks_file_path = match env::var("SHMARKS_LIST_FILE") {
         Ok(value) => PathBuf::from(value),
-        Err(_) => dirs::config_local_dir().expect("Cannot resolve default config directory. Set 'SHMARKS_LIST_FILE' environment variable").join("shmarks.toml"),
+        Err(_) => dirs::config_local_dir().ok_or_else(|| 
+            Error::Msg(
+                "Cannot resolve default config directory. Set 'SHMARKS_LIST_FILE' environment variable"
+                .to_string()
+            )
+        )?,
     };
 
-    if !shmarks_file_path.parent().unwrap().exists() {
+    if !shmarks_file_path.is_file() {
         return Err(Error::Msg(
-            "Directory of shmarks list file doesn't exist".to_string(),
+            format!("'SHMARKS_LIST_FILE' is not a file. Provided path: {}", 
+            shmarks_file_path.to_string_lossy())
         ));
     }
 
-    let toml_str = util::read_contents_file(&shmarks_file_path).map_err(|err| {
-        format!(
-            "Failed reading '{}': {}",
-            shmarks_file_path.to_str().unwrap(),
-            err
-        )
-    })?;
+    let mut ads: AliasesDirs = {
+        let toml_str = util::read_contents_file(&shmarks_file_path).map_err(|err| {
+            format!(
+                "Failed reading '{}': {}",
+                shmarks_file_path.to_str().unwrap(),
+                err
+            )
+        })?;
+    
+        let toml: toml::Value = toml::from_str(&toml_str).map_err(|err| {
+            format!(
+                "Failed parsing toml from '{}': {}",
+                shmarks_file_path.to_str().unwrap(),
+                err
+            )
+        })?;
 
-    let toml: toml::Value = toml::from_str(&toml_str).map_err(|err| {
-        format!(
-            "Failed parsing toml from '{}': {}",
-            shmarks_file_path.to_str().unwrap(),
-            err
-        )
-    })?;
+        toml_parser::to_btreemap(&toml).map_err(|err| {
+            format!(
+                "Failed processing toml from '{}': {}",
+                shmarks_file_path.to_str().unwrap(),
+                err
+            )
+        })?
+    };
 
-    let mut toml_map: AliasesDirs = toml_parser::to_btreemap(&toml).map_err(|err| {
-        format!(
-            "Failed processing toml from '{}': {}",
-            shmarks_file_path.to_str().unwrap(),
-            err
-        )
-    })?;
+    fn replace_contents_of_toml(ads: &AliasesDirs, shmarks_file_path: &PathBuf) -> Result<()> {
+        let toml_new_string = toml::to_string_pretty(
+            &toml_parser::to_toml(&ads)
+        )?;
+    
+        util::replace_contents_of_file(&shmarks_file_path, &toml_new_string).map_err(|e| {
+            format!("Failed replacing contents of '{}': {}", shmarks_file_path.to_string_lossy(), e)
+        })?;
+        
+        Ok(())
+    }
+
+    let matches = app::matches();
 
     match matches.subcommand() {
         Some(("new", sub_m)) => {
-            process_subcommand::new(&sub_m, &mut toml_map)?;
-            replace_contents_of_toml(&toml_map, &shmarks_file_path)?;
+            process_subcommand::new(&sub_m, &mut ads)?;
+            replace_contents_of_toml(&ads, &shmarks_file_path)?;
         }
         Some(("rm", sub_m)) => {
-            process_subcommand::rm(&sub_m, &mut toml_map)?;
-            replace_contents_of_toml(&toml_map, &shmarks_file_path)?;
+            process_subcommand::rm(&sub_m, &mut ads)?;
+            replace_contents_of_toml(&ads, &shmarks_file_path)?;
         }
-        Some(("ls", sub_m)) => process_subcommand::ls(&sub_m, &mut toml_map),
-        _ => process_subcommand::none(&matches, &toml_map, &shmarks_file_path)?,
+        Some(("ls", sub_m)) => process_subcommand::ls(&sub_m, &mut ads),
+        _ => process_subcommand::none(&matches, &ads, &shmarks_file_path)?,
     }
 
-    Ok(())
-}
-
-fn replace_contents_of_toml(toml_map: &AliasesDirs, shmarks_file_path: &PathBuf) -> Result<()> {
-    let toml_new = toml_parser::to_toml(&toml_map);
-    let toml_new_string = toml::to_string_pretty(&toml_new)?;
-
-    util::replace_contents_of_file(&shmarks_file_path, &toml_new_string)?;
     Ok(())
 }
--- src/process_subcommand.rs
+++ src/process_subcommand.rs
@@ -8,31 +8,34 @@ use std::path::PathBuf;
 use std::process;
 
 pub fn ls(m: &ArgMatches, toml_map: &mut AliasesDirs) {
-    let alias_color = nu_ansi_term::Color::LightCyan;
-
-    if m.get_flag("directory") {
-        let color_len = alias_color.paint(".").to_string().len() - 1;
-        let max_length = toml_map.keys().map(|s| s.len()).max().unwrap_or(0);
-
-        for key in toml_map.keys() {
-            let padding = max_length - key.len() + color_len;
-            let formatted_string = format!(
-                "{:<width$}   {}",
-                alias_color.paint(key).to_string(),
-                toml_map[key].to_string_lossy(),
-                width = key.len() + padding
-            );
-            println!("{}", formatted_string);
-        }
-    } else {
+    // Simple print like "a1 a2 a3\n"
+    if !m.get_flag("directory") {
+        let keys_len = toml_map.keys().len();
         for (index, element) in toml_map.keys().enumerate() {
-            if index < toml_map.keys().len() - 1 {
+            if index < keys_len - 1 {
                 print!("{} ", element);
             } else {
                 print!("{}", element);
             }
         }
         println!();
+        return;
+    }
+
+    // Colored print in two columns
+    let alias_color = nu_ansi_term::Color::LightCyan;
+    let color_len = alias_color.paint(".").to_string().len() - 1;
+    let max_length = toml_map.keys().map(|s| s.len()).max().unwrap_or(0);
+
+    for key in toml_map.keys() {
+        let padding = max_length - key.len() + color_len;
+        let formatted_string = format!(
+            "{:<width$}   {}",
+            alias_color.paint(key).to_string(),
+            toml_map[key].to_string_lossy(),
+            width = key.len() + padding
+        );
+        println!("{}", formatted_string);
     }
 }
 
@@ -57,8 +60,10 @@ pub fn rm(m: &ArgMatches, toml_map: &mut AliasesDirs) -> Result<()> {
 
 pub fn new(m: &ArgMatches, toml_map: &mut AliasesDirs) -> Result<()> {
     let alias_arg = m.get_one::<String>("alias").unwrap();
-    let path_arg = m.get_one::<PathBuf>("directory").unwrap();
-    let absolute_path_arg = normalize::abs_normalize_path(path_arg)?;
+    let absolute_path_arg = {
+        let path_arg = m.get_one::<PathBuf>("directory").unwrap();
+        normalize::abs_normalize_path(path_arg)?
+    };
 
     toml_map.insert(alias_arg.to_string(), absolute_path_arg);
     Ok(())
@@ -66,28 +71,41 @@ pub fn new(m: &ArgMatches, toml_map: &mut AliasesDirs) -> Result<()> {
 
 pub fn none(m: &ArgMatches, toml_map: &AliasesDirs, shmarks_file_path: &PathBuf) -> Result<()> {
     if let Some(alias) = m.get_one::<String>("alias") {
-        let dir_to_set = &toml_map.get(alias);
+        let dir_to_set = toml_map.get(alias);
         if let Some(dir) = dir_to_set {
             println!("{}", dir.to_string_lossy());
-        } else {
-            return Err(Error::Msg(format!("Alias '{}' not found", alias)));
+            return Ok(());
         }
-    } else if m.get_flag("edit") {
+
+        return Err(Error::Msg(format!("Alias '{}' not found", alias)));
+    }
+
+    if m.get_flag("edit") {
         let editor = env::var("EDITOR").unwrap_or_else(|_| String::from("vi"));
         process::Command::new(&editor)
             .arg(&shmarks_file_path)
             .status()
-            .expect("Failed to open the file in the editor");
-    } else {
-        let default_alias_name = "_default";
-        if let Some(dir) = &toml_map.get(default_alias_name) {
-            println!("{}", dir.to_string_lossy());
-        } else {
-            return Err(Error::Msg(format!(
-                "No default directory was set, add alias '{}' to cd into default directory",
-                default_alias_name
-            )));
-        }
+            .map_err(|err| {
+                format!(
+                    "Failed opening file '{}' in editor '{}': {}",
+                    shmarks_file_path.to_string_lossy(),
+                    editor,
+                    err
+                )
+            })?;
+
+        return Ok(());
     }
+
+    let default_alias_name = "_default";
+    if let Some(dir) = toml_map.get(default_alias_name) {
+        println!("{}", dir.to_string_lossy());
+    } else {
+        return Err(Error::Msg(format!(
+            "No default directory was set, add alias '{}' to cd into default directory",
+            default_alias_name
+        )));
+    }
+
     Ok(())
 }
